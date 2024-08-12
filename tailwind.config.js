4238e14c64a4e1d52cc214301dd605b551948152
ij2002 <ij2002@hw.ac.uk>
2024-08-12 16:35:35 +0530

    fixed navbar on mobile, ensured sizes match when scrolled, fixed placement of transparent navbar to maintain consistency, added a translucent bg to the transparent navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./app/**/*.{html,js,razor}"],
   theme: {
+    screens: {
+      'xs': '450px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       colors: {
         primary: {
