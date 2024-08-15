a4ac73546836aecb131c2b9182ffa0ae671df731
Djoufson CHE BENE <djouflegran@gmail.com>
2024-08-15 09:40:14 +0100

    #39 Implement the AdminsSeeder

---
+++ app/Persistence/Seeders/AdminsSeeder.cs
@@ -0,0 +1,24 @@
+using app.Models.Identity;
+using app.Utils;
+using EntityFrameworkCore.Seeder.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace app.Persistence.Seeders;
+public class AdminsSeeder(UserManager<User> userManager) : ISeeder
+{
+    private readonly UserManager<User> _userManager = userManager;
+
+    public async Task SeedAsync()
+    {
+        var admin = new User()
+        {
+            UserName = "admin",
+            Email = "admin@mail.com",
+            EmailConfirmed = true,
+
+        };
+
+        await _userManager.CreateAsync(admin, "Admin@123");
+        await _userManager.AddToRoleAsync(admin, Roles.Admin);
+    }
+}
