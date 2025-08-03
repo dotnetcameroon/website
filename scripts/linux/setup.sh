7e156c96a054a9df82354f8ab51679ea9267c8af
Djoufson CHE BENE <djouflegran@gmail.com>
2025-08-03 08:32:49 +0100

    fix setup scripts

--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -2,16 +2,16 @@
 docker compose up -d
 
 # Wait until the SQL Server container is ready
-until docker exec sqlserver /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P '!Passw0rd' -Q "SELECT 1" -C &> /dev/null; do
+until docker exec sqlserver /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P '@Passw0rd' -Q "SELECT 1" -C &> /dev/null; do
   echo "Waiting for SQL Server to be ready..."
   sleep 3
 done
 
 # Migrate the database
-dotnet ef database update -s ./app -p ./app.infrastructure --context AppDbContext && \
+dotnet ef database update -s ./src//app -p ./src/app.infrastructure --context AppDbContext && \
 
 # Seed the database
-dotnet run --project ./app/ --seed RolesSeeder PartnersSeeder EventsSeeder AdminsSeeder ProjectsSeeder
+dotnet run --project ./src/app/ --seed RolesSeeder PartnersSeeder EventsSeeder AdminsSeeder ProjectsSeeder
 
 # Shut down the database
 docker compose down
